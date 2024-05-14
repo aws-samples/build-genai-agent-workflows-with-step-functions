5b787bf49233e9e3ae2076a92d5d5afd596be645
Harold Sun <sunhua@amazon.com>
2024-05-14 10:17:42 +0000

    Switch to mistral model

--- functions/generic/format_prompt/index.py
+++ functions/generic/format_prompt/index.py
@@ -6,15 +6,16 @@ logger = logging.getLogger()
 
 def apply_prompt_template(messages):
     # Define the Jinja2 template
-    llama2_prompt_template = """\
+    prompt_template = """\
 {% if messages[0]['role'] == 'system' %}
     {% set loop_messages = messages[1:] %}
-    {% set system_message = '<<SYS>>\n' + messages[0]['content'].strip() + '\n<</SYS>>\n\n' %}
+    {% set system_message = messages[0]['content'].strip() + '\n\n' %}
 {% else %}
     {% set loop_messages = messages %}
     {% set system_message = '' %}
 {% endif %}
 
+{{ bos_token }}
 {% for message in loop_messages %}
     {% if (message['role'] == 'user') != (loop.index0 % 2 == 0) %}
         {{ raise_exception('Conversation roles must alternate user/assistant/user/assistant/...') }}
@@ -27,14 +28,14 @@ def apply_prompt_template(messages):
     {% endif %}
     
     {% if message['role'] == 'user' %}
-        {{ bos_token + '[INST] ' + content.strip() + ' [/INST]' }}
+        {{ '[INST] ' + content.strip() + ' [/INST]' }}
     {% elif message['role'] == 'assistant' %}
-        {{ ' '  + content.strip() + ' ' + eos_token }}
+        {{ ' ' + content.strip() + eos_token }}
     {% endif %}
 {% endfor %}
 """
 
-    template = jinja2.Template(llama2_prompt_template)
+    template = jinja2.Template(prompt_template)
     rendered_prompt = template.render(
         bos_token="<s>", eos_token="</s>", messages=messages
     )
